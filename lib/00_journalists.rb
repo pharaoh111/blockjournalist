6ed46046dd28e1dae710c14b64ebbe9cff8fae7c
pharaoh111 <60857611+pharaoh111@users.noreply.github.com>
2021-01-14 20:48:08 +0100

    Add files via upload

---
+++ lib/00_journalists.rb
@@ -0,0 +1,144 @@
+accounts = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@min","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@ceci_est_un_handle_vraiment_long","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+
+#----------------------------------------------------------------------------------------------------------------#
+# Pour mesurer la length des string
+
+def longueur(mesure)
+    puts mesure.length
+end
+#----------------------------------------------------------------------------------------------------------------#
+# Choose the shortest in the list
+
+def short(shorter)
+    liluser = shorter.max_by { |x| -x.length }
+    puts liluser
+end
+#----------------------------------------------------------------------------------------------------------------#
+# Choose Only Five Letters
+
+def five(fiver)
+
+array = []
+
+fiver.each do |item|
+        
+        if item.length == 6
+            array.push(item)
+        end
+    end
+    puts "here #{array}"
+end
+#----------------------------------------------------------------------------------------------------------------#
+# Put in the correct alphabetical order
+
+
+def alphabet(alpha)
+    alpha.sort_by!{ |i| i.downcase }
+    puts alpha
+end
+#----------------------------------------------------------------------------------------------------------------#
+# How many handles starts with a capital letter
+
+def uppercase(upper)
+
+    puts upper.count{|handle| handle[1].match?( /[[:upper:]]/ )}
+
+end
+#----------------------------------------------------------------------------------------------------------------#
+# Sorts the handle list by handle size (smallest first, largest after)
+def sizer(size)
+
+    size = size.sort_by(&:length)
+    puts size
+end
+#----------------------------------------------------------------------------------------------------------------#
+# What is the position in the array of the person @epenser
+def position(find)
+    puts find.index("@epenser")
+end
+#----------------------------------------------------------------------------------------------------------------#
+# Get me a distribution of handles by size of the latter 
+#(number of handles with 1 character, number of handles with 2 characters, number of handles with 3 characters, etc.)
+def repartition(division)
+
+    arraz = division.sort_by(&:length)
+    
+   #arraz.each do |item|
+       
+        array4 = ["#------------number of handle with 4 digits + the list of theses handles------------#\n\n"]
+        array5 = ["#------------number of handle with 5 digits + the list of theses handles------------#\n\n"]
+        array6 = ["#------------number of handle with 6 digits + the list of theses handles------------#\n\n"]
+        array7 = ["#------------number of handle with 7 digits + the list of theses handles------------#\n\n"]
+        array8 = ["#------------number of handle with 8 digits + the list of theses handles------------#\n\n"]
+        array9 = ["#------------number of handle with 9 digits + the list of theses handles------------#\n\n"]
+        array10 = ["#------------number of handle with 10 digits + the list of theses handles------------#\n\n"]
+        array11 = ["#------------number of handle with 11 digits + the list of theses handles------------#\n\n"]
+        array12 = ["#------------number of handle with 12 digits + the list of theses handles------------#\n\n"]
+        array13 = ["#------------number of handle with 13 digits + the list of theses handles------------#\n\n"]
+        array14 = ["#------------number of handle with 14 digits + the list of theses handles------------# \n\n"]
+        
+        arraz.each do |item|
+        
+        if item.length == 4
+            array4.push(item)
+        end
+        if item.length == 5
+            array5.push(item)
+        end
+        if item.length == 6
+            array6.push(item)
+        end
+        if item.length == 7
+            array7.push(item)
+        end
+        if item.length == 8
+            array8.push(item)
+        end
+        if item.length == 9
+            array9.push(item)
+        end
+        if item.length == 10
+            array10.push(item)
+        end
+        if item.length == 11
+            array11.push(item)
+        end
+        if item.length == 12
+            array12.push(item)
+        end
+        if item.length == 13
+            array13.push(item)
+        end
+        if item.length == 14
+            array14.push(item)                     
+        end
+
+    
+    end
+    
+    puts array4, array4.length - 1 , 
+            array5, array5.length - 1, array6,
+            array6.length - 1, 
+            array7, array7.length - 1,
+            array8, array8.length - 1,
+            array9, array9.length - 1,
+            array10, array10.length - 1,
+            array11, array11.length - 1,
+            array12, array12.length - 1,
+            array13, array13.length - 1,
+            array14, array14.length - 1
+
+end
+
+
+
+
+
+#longueur(accounts)
+#short(accounts)
+#alphabet(accounts)
+#five(accounts)
+#uppercase(accounts)
+#sizer(accounts)
+#position(accounts)
+repartition(accounts)
